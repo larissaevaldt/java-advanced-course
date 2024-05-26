bc9cd541ef87eabddaec78f81836ade79df162e0
larissaevaldt <lari.justo@gmail.com>
2024-05-26 17:46:49 +0100

    corrections

--- src/main/java/org/example/lambda/assignment/AssignmentOne.java
+++ src/main/java/org/example/lambda/assignment/AssignmentOne.java
@@ -28,29 +28,27 @@ interface Functionable<T, R> {
 public class AssignmentOne {
 
     public static void main(String[] args) {
-        consumer();
-        supplier();
-        predicate();
-        function();
+        AssignmentOne a1 = new AssignmentOne();
+        a1.consumer();
+        a1.supplier();
+        a1.predicate();
+        a1.function();
 
         // 5) invoke the getPeople() – store the result in a variable named listPeople.
         List<Person> listPeople = getPeople();
-
         // 6) invoke the sortAge() method passing down listPeople;
         sortAge(listPeople);
-
         // 7) invoke the sortName() method passing down listPeople
         sortName(listPeople);
-
         // 8) invoke the sortHeight() method passing down listPeople
         sortHeight(listPeople);
     }
 
-    public static void consumer() {
+    public void consumer() {
         // a) Using a lambda expression, implement the Printable interface (typed for String).
         // The relevant method just prints out the String argument it receives.
         // Invoke the relevant method, passing in "Printable lambda".
-        Printable<String> lambdaI = (String str) -> System.out.println(str);
+        Printable<String> lambdaI = str -> System.out.println(str);
         String s = "Printable lambda";
         lambdaI.print(s);
 
@@ -61,7 +59,7 @@ public class AssignmentOne {
         printCMR.accept(s);
     }
 
-    public static void supplier() {
+    public void supplier() {
         // a) Using a lambda expression, implement the Retrievable interface (typed for Integer).
         // The relevant method just returns 77.
         // Invoke the relevant method
@@ -75,7 +73,7 @@ public class AssignmentOne {
         System.out.println(i2);
     }
 
-    public static void predicate() {
+    public void predicate() {
         // a) Using a lambda expression, implement the Evaluate interface (typed for Integer).
         // The relevant method returns true if the argument passed is < 0, otherwise it returns false.
         // Invoke the relevant method twice – the first time pass in -1 and the second time pass in +1
@@ -91,26 +89,30 @@ public class AssignmentOne {
         // c) Invoke the check() method with the following Predicate lambda expressions:
         // we want to know if a number is even (true)
         Predicate<Integer> predicate1 = i -> i % 2 == 0;
-        System.out.println(check(4, predicate1)); // true
-        System.out.println(check(7, predicate1)); // false
+        System.out.println(check(4, predicate1));      // true
+        System.out.println(check(7, i -> i % 2 == 0)); // false
+
+        // we want to know if a String begins with “Mr.”
+        System.out.println(check("Mr. Joe Bloggs", s -> s.startsWith("Mr."))); // true
+        System.out.println(check("Ms. Ann Bloggs", s -> s.startsWith("Mr."))); // false
 
         // we want to know if a person is an adult (age >= 18)
         Person p1 = new Person("Mike", 33, 1.8);
         Person p2 = new Person("Ann", 13, 1.4);
-        Predicate<Person> predicate2 = p -> p.getAge() > 18;
-        System.out.println(check(p1, predicate2)); // true
-        System.out.println(check(p2, predicate2)); // false
+        Predicate<Person> predicate2 = p -> p.getAge() >= 18;
+        System.out.println(check(p1, predicate2));            // true
+        System.out.println(check(p2, p -> p.getAge() >= 18)); // false
     }
 
 
     // 3c) Declare a generically-typed check() method (not in UML).
     // The first parameter is generic and the second parameter is a Predicate, also generically typed.
     // The check() method returns true/false
-    private static <T> boolean check(T t, Predicate<T> predicate) {
+    public static <T> boolean check(T t, Predicate<T> predicate) {
         return predicate.test(t);
     }
 
-    public static void function() {
+    public void function() {
         // a) Using a lambda expression, implement the Functionable interface - the input type is Integer and the return type is String.
         // The relevant method returns the number passed in appended to the String “Number is: ”.
         // Invoke the relevant method passing in 25.
@@ -131,31 +133,43 @@ public class AssignmentOne {
         return result;
     }
 
-    private static void sortAge(List<Person> people) {
+    private static void sortAge(List<Person> listPeople) {
         // a) Using the Iterable sort() method (note: List extends Iterable), and the Comparator.comparing() method, sort the Person objects in ascending age order
-        people.sort(Comparator.comparing(Person::getAge));
+        listPeople.sort(
+                Comparator.comparing(p -> p.getAge()));  // lambda syntax
+                //Comparator.comparing(Person::getAge)); // method reference syntax
+
         // b) Output the sorted list using the Iterable forEach() method passing in a lambda expression.
-        //people.forEach(p -> System.out.println(p));
+        //listPeople.forEach(p -> System.out.println(p)); //lambda syntax
+
         // 9. Refactor 6b, 7b and 8b to use method references instead of lambda expressions.
-        people.forEach(System.out::println);
+        listPeople.forEach(System.out::println);
     }
 
-    private static void sortName(List<Person> people) {
+    private static void sortName(List<Person> listPeople) {
         // a) As in 6a except sort the Person objects in ascending name order.
-        people.sort(Comparator.comparing(Person::getName));
+        listPeople.sort(
+                Comparator.comparing(p -> p.getName()));  // lambda syntax
+                //Comparator.comparing(Person::getName)); // method reference syntax
+
         // b) Output the sorted list using the Iterable forEach() method passing in a lambda expression
-        //people.forEach(p -> System.out.println(p));
+        //listPeople.forEach(p -> System.out.println(p)); // lambda syntax
+
         // 9. Refactor 6b, 7b and 8b to use method references instead of lambda expressions.
-        people.forEach(System.out::println);
+        listPeople.forEach(System.out::println);
     }
 
-    private static void sortHeight(List<Person> people) {
+    private static void sortHeight(List<Person> listPeople) {
         // a) As in 6a except sort the Person objects in ascending height order.
-        people.sort(Comparator.comparing(Person::getHeight));
+        listPeople.sort(
+                Comparator.comparing(p -> p.getHeight()));   // lambda syntax
+                // Comparator.comparing(Person::getHeight)); // method reference syntax
+
         // b) Output the sorted list using the Iterable forEach() method passing in a lambda expression.
-        //people.forEach(p -> System.out.println(p));
+        //listPeople.forEach(p -> System.out.println(p));  // lambda syntax
+
         // 9. Refactor 6b, 7b and 8b to use method references instead of lambda expressions.
-        people.forEach(System.out::println);
+        listPeople.forEach(System.out::println);
     }
 }
 
